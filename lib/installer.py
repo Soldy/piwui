cf660090ce3f032b4a4f00e808e30b9b273ea88f
testa <testa.a.tapos@gmail.com>
2013-08-03 01:49:40 +0100

    Add installer

---
+++ lib/installer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import osonsql
+
+
+osondbcon = osonsql.connect("sqlite", "db/file.db")
+tableaccesn = ("id", "filepath", "filename", "filetype", "filesize", "fileaudioform", "fileaudiobit", "fileaudiorate", "fileaudiochannel", "fileaudiolangs", "filevideoformat", "filevideobit", "filevideoresult", "filevideofps", "filevideoaspect", "filelength", "fileseekable", "filechapters", "filemd5", "fileid") 
+tableaccest = ("text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text")
+tableaccess = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+osonsql.make("sqlite", osondbcon, "files", tableaccesn, tableaccest, tableaccess)
+osondbcon = osonsql.connect("sqlite", "db/passwd.db")
+tableaccesn = ("id", "userid", "username", "password") 
+tableaccest = ("text", "text", "text", "text")
+tableaccess = (0, 0, 0, 0)
+osonsql.make("sqlite", osondbcon, "passwd", tableaccesn, tableaccest, tableaccess)
